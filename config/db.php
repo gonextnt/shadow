6ca5c5a5e836b8c8d67db1ad38a5b0f3da154dd5
gonextnt <ivan1999herok@gmail.com>
2026-03-25 11:40:25 +0700

    bd and singup

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'pcymitci_m4',
+    'username' => 'pcymitci',
+    'password' => 'uyT1eN',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
