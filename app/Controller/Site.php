aaf5e04eaaf1169c3661250f53c29ec4e0074e71
gonextnt <ivan1999herok@gmail.com>
2026-03-26 09:42:07 +0700

    login and logout, Auth

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -2,7 +2,7 @@
 
 namespace Controller;
 
-
+use Src\Auth\Auth;
 use Model\Post;
 use Src\View;
 use Src\Request;
@@ -24,9 +24,29 @@ class Site
 
     public function signup(Request $request): string
     {
-        if ($request->method==='POST' && User::create($request->all())){
-            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
         }
         return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        //Если просто обращение к странице, то отобразить форму
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+        //Если удалось аутентифицировать пользователя, то редирект
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+        //Если аутентификация не удалась, то сообщение об ошибке
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
